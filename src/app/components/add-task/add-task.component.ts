c3344fea99d87c34a53c7ed68a21bbbaa716d1ca
Ali Haider <ali.haider@sigmatec.com.pk>
2022-05-17 13:36:23 +0500

    Toggle Add Component

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,6 +1,7 @@
-import { IfStmt } from '@angular/compiler';
 import { Component, OnInit,Input, Output,EventEmitter } from '@angular/core';
 import { Task } from '../..//Task';
+import { Subscription } from 'rxjs';
+import { UiService } from '../../service/ui.service';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
@@ -11,7 +12,11 @@ export class AddTaskComponent implements OnInit {
   text! :string;
   day! :string;
   reminder: boolean = false;
-  constructor() { }
+  showAddTask : boolean = false;
+  subscription :Subscription;
+  constructor(private uiService : UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => this.showAddTask = value);
+   }
 
   ngOnInit(): void {
   }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiService } from '../../service/ui.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
   title :string = "Header Works";
-  constructor() { }
+  showAddTask : boolean = false;
+  subscription: Subscription;
+  constructor(private uiService : UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => this.showAddTask = value);
+   }
 
   ngOnInit(): void {
   }
 
   toggleAddTask(){
-   alert("Hello World");
+   this.uiService.showToggleTask();
   }
 }
